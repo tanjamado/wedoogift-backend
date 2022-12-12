a6bb1289090f97f7f17065207173fdb3c2fdd089
Othmane <allamouothmane50@gmail.com>
2022-12-12 19:21:22 +0100

    initial commit

---
+++ src/main/java/com/allamou/wedoogiftbackend/WedoogiftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.allamou.wedoogiftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedoogiftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WedoogiftBackendApplication.class, args);
+    }
+
+}
